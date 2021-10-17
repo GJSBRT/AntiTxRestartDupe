18044850d14def040918ddef43d3e08807706e14
GJSBRT <gijsgames9@gmail.com>
2021-10-17 14:50:43 +0200

    Now kicks everyone before restart

--- server.lua
+++ server.lua
@@ -4,9 +4,20 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 AddEventHandler('txAdmin:events:scheduledRestart', function(eventData)
     if eventData.secondsRemaining == 60 then
         CreateThread(function()
-            Wait(57000)
-            print("3 seconds before restart... saving all players!")
+
+            Wait(57000) -- Wainting till the final 3 seconeds before restart 
+
+            -- Sends message to console and saves all players
+            print("3 seconds before restart... saving and kicking all players!")
             ESX.SavePlayers()
+
+            Wait(2000) -- Waiting for all players to finish saving
+
+            -- Kicks everyone from the server
+            for i=1, #xPlayers, 1 do
+                local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+                xPlayer.kick("The server is restarting, come back later.")
+            end
         end)
     end
 end)
